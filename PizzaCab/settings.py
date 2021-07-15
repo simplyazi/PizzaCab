eb5e46ab8eed4e23356dcd78cdc3fe1656f3d8c6
simplyazi <johnazarel@gmail.com>
2021-07-15 08:42:15 +0800

    Update settings.py

--- PizzaCab/settings.py
+++ PizzaCab/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path,os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 
@@ -24,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mc%-=kjits9yhz5$5tr7)&2)rv0mu92@2ou(^d&q7i-xos*^=^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','pizzacab.herokuapp.com']
 
 
 # Application definition
@@ -119,6 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
 
